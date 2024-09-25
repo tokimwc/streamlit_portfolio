c6f039d7b625ff506bdaf51393e15fae8ee15afb
murakami <murakami@astom.co.jp>
2024-09-25 11:22:22 +0900

    Update button and checkbox examples with unique keys

--- app.py
+++ app.py
@@ -283,3 +283,39 @@ st.write(st.session_state.df)
 if st.button("データをリセット"):
  st.session_state.df = pd.DataFrame(columns=['商品', '価格'])
  st.rerun()
+
+st.header('レッスン10: ボタンとチェックボックス')
+if st.button('データを⽣成', key='generate_data'):
+ random_data = pd.DataFrame(np.random.randn(20, 3), columns=['X', 'Y',
+'Z'])
+ st.write(random_data)
+
+show_chart = st.checkbox('チャートを表⽰', key='show_chart')
+if show_chart:
+    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['X', 'Y',
+'Z'])
+    fig = go.Figure()
+    for column in chart_data.columns:
+        fig.add_trace(go.Scatter(x=chart_data.index, y=chart_data[column],
+mode='lines', name=column))
+    st.plotly_chart(fig)
+
+if 'counter' not in st.session_state:
+ st.session_state.counter = 0
+col1, col2, col3 = st.columns(3)
+if col1.button('カウントアップ', key='count_up'):
+ st.session_state.counter += 1
+if col2.button('カウントダウン', key='count_down'):
+ st.session_state.counter -= 1
+if col3.button('リセット', key='reset_count'):
+ st.session_state.counter = 0
+st.write(f"現在のカウント: {st.session_state.counter}")
+
+column_options = st.multiselect(
+ '表⽰する列を選択してください',
+ ['X', 'Y', 'Z'],
+ ['X', 'Y', 'Z'],
+ key='column_selection')
+sample_data = pd.DataFrame(np.random.randn(10, 3), columns=['X', 'Y',
+'Z'])
+st.write(sample_data[column_options])
